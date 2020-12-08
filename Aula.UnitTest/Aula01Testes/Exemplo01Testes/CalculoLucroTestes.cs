4936c0c9e6ba2daf44bc9c8380a426a25299dff7
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-08 20:40:54 -0300

    Implementação de Testes com Erro #1
    Implementação dos testes com erro.
    Throw new NotImplementedException

---
+++ Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoLucroTestes.cs
@@ -0,0 +1,45 @@
+﻿using Aula.Lib.Aula01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Aula.UnitTest.Aula01Testes.Exemplo01Testes
+{
+    public class CalculoLucroTestes
+    {
+        [Fact]
+        public void CalculoLucroExemploExterno()
+        {
+            //https://blog.contaazul.com/saiba-como-calcular-a-margem-de-lucro-de-um-produto-e-da-empresa#:~:text=Basicamente%2C%20o%20lucro%20%C3%A9%20a,%3D%20receitas%20totais%20%E2%80%93%20custos).
+            decimal mk = Exemplo01.CalculaLucro(13.000M, 20.000M);
+
+            Assert.Equal(7.000M, mk);
+        }
+        [Fact]
+        public void CalculaTesteProprio()
+        {
+            decimal mk = Exemplo01.CalculaLucro(320M, 540M);
+
+            Assert.Equal(220M, mk);
+        }
+        [Fact]
+        public void CalculaPVZero()
+        {
+            decimal mk = Exemplo01.CalculaLucro(pc: 13.000M, 0M);
+
+            Assert.Equal(0, mk);
+        }
+        [Fact]
+        public void CalculaPCZero()
+        {
+            decimal mk = Exemplo01.CalculaLucro(0M, 100M);
+
+            Assert.Equal(mk, mk);
+        }
+
+
+    }
+}
--- Aula.lib/Aula01/Exemplo01.cs
+++ Aula.lib/Aula01/Exemplo01.cs
@@ -6,11 +6,8 @@ namespace Aula.Lib.Aula01
     {
         public static decimal CalcularMarkup(decimal PC, decimal PV)
         {
-            if (PC == 0)
-            {
-                return 0;
-            }
-
+            if (PC == 0) { return 0; }
+            
             decimal mk = PV / PC;
 
             return mk;
@@ -20,7 +17,6 @@ namespace Aula.Lib.Aula01
         public static decimal CalculaLucro(decimal pc, decimal pv)
         {
             throw new NotImplementedException();
-            
         }
 
         public static decimal CalculaPV_MK(decimal pc, decimal mk)

c3330a65562dd050c217db44121d4ba401018813
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-09 15:59:51 -0300

    Implementação falha de cálculo do pv via MLC #3

--- Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoPVporMargem.cs
+++ Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoPVporMargem.cs
@@ -17,6 +17,18 @@ namespace Aula.UnitTest.Aula01Testes.Exemplo01Testes
 
             Assert.Equal(62.5M, pv);
         }
+        [Fact]
+        public void CalculaPVMLCExemplo()
+        {
+            //https://blog.contaazul.com/saiba-como-calcular-a-margem-de-lucro-de-um-produto-e-da-empresa#:~:text=O%20c%C3%A1lculo%20da%20margem%20de,13%20mil%20no%20mesmo%20per%C3%ADodo.
+
+            decimal pv = Exemplo01.CalculaPV_MLC(13.000M, 35M);
+
+            Assert.Equal(7500M, pv);
+            
+        
+        }
+
 
 
 

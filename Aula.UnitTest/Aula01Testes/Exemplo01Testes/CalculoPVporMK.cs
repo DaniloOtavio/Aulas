12709a225a565978623d20e5b830ff2e0ee75994
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-08 21:01:25 -0300

    Implementação de Testes com Erro #2
    Implementação dos testes com o not implemented exception

---
+++ Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoPVporMK.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Aula.Lib.Aula01;
+
+namespace Aula.UnitTest.Aula01Testes.Exemplo01Testes
+{
+    public class CalculoPVporMK
+    {
+        [Fact]
+        public void CalculoPCZero()
+        {
+            decimal mk = Exemplo01.CalculaPV_MK(0M, 1.35M);
+
+            Assert.Equal(0, mk);
+        }[Fact]
+        public void CalculoMKZero()
+        {
+            decimal mk = Exemplo01.CalculaPV_MK(100M, 0);
+
+            Assert.Equal(0, mk);
+        }
+        [Fact]
+        public void CalculoExemploExterno()
+        {
+            //https://fia.com.br/blog/markup/
+            decimal mk = Exemplo01.CalculaPV_MK(45M, 2.04M);
+
+            Assert.Equal(91.83M, mk);
+        }
+        [Fact]
+        public void CalculoExemploProprio()
+        {
+            decimal mk = Exemplo01.CalculaPV_MK(500, 1.33M);
+
+            Assert.Equal(665M, mk);
+        }
+    }
+}

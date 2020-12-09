5ffa0ee24e038e7e9cf44b0c94210e1b57834d0c
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-09 16:05:00 -0300

    Correção Função CalculaPV_MLC #3
    Underline = separação de milhar

--- Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoPVporMargem.cs
+++ Aula.UnitTest/Aula01Testes/Exemplo01Testes/CalculoPVporMargem.cs
@@ -22,11 +22,9 @@ namespace Aula.UnitTest.Aula01Testes.Exemplo01Testes
         {
             //https://blog.contaazul.com/saiba-como-calcular-a-margem-de-lucro-de-um-produto-e-da-empresa#:~:text=O%20c%C3%A1lculo%20da%20margem%20de,13%20mil%20no%20mesmo%20per%C3%ADodo.
 
-            decimal pv = Exemplo01.CalculaPV_MLC(13.000M, 35M);
+            decimal pv = Exemplo01.CalculaPV_MLC(13_000M, 35M);
 
-            Assert.Equal(7500M, pv);
-            
-        
+            Assert.Equal(17550M, pv);
         }
 
 

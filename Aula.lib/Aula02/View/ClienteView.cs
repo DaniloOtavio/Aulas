b057ed5265c8d67fd5af3787f25107c2e49c83aa
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-16 09:55:22 -0300

    Criação da View para representar Clientes #8
    View herdando de ClientesDB

---
+++ Aula.lib/Aula02/View/ClienteView.cs
@@ -0,0 +1,12 @@
+﻿using Aula.Lib.Aula02.Models_DB;
+
+namespace Aula.Lib.Aula02.View
+{
+    /// <summary>
+    /// Classe para representar a view do cliente, herdando de ClienteDB
+    /// </summary>
+    public class ClienteView : ClienteDB
+    {
+
+    }
+}

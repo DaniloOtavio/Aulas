ca6d7edaf954661f38aff842fb8f2083ec6dee52
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-16 13:32:31 -0300

    Texto sobre OOP - Herança #10
    Fonte: https://www.caelum.com.br/apostila-csharp-orientacao-objetos/heranca

---
+++ Aula.lib/Aula02/OOPHeranca/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Lib.Aula02.OOP___Herança
+{
+    /// <summary>
+    /// Classe pai de Conta - https://www.caelum.com.br/apostila-csharp-orientacao-objetos/heranca#polimorfismo
+    /// </summary>
+    public class Conta
+    {
+        /// <summary>
+        /// Valor
+        /// </summary>
+        public int Numero { get; set; }
+        /// <summary>
+        /// Saldo
+        /// </summary>
+        public double Saldo { get; private set; }
+        /// <summary>
+        /// CPF do titular
+        /// </summary>
+        public string ClienteTitular { get; set; }
+
+        /// <summary>
+        /// Sacar
+        /// </summary>
+        /// <param name="valor"></param>
+        public virtual void Saca(double valor)
+        {
+            this.Saldo -= valor;
+        }
+
+        /// <summary>
+        /// Depositar
+        /// </summary>
+        /// <param name="valor"></param>
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
---
+++ Aula.lib/Aula02/OOPHeranca/ContaPoupanca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Lib.Aula02.OOP___Herança
+{
+    class ContaPoupanca : Conta
+    {
+        public override void Saca(double valor)
+        {
+            //SE USAR AQUI PRECISA MUDAR A PROPRIEDADE DE SALDO DE PRIVATE PARA PROTECTED
+            //this.Saldo -= (valor + 0.10);
+            base.Saca(valor + 0.10);
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+}

3cfa9559c01b0d28c1cd3d1717f284e1c30e4227
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-16 09:52:07 -0300

    Criação da classe Model para Clientes #8

---
+++ Aula.lib/Aula02/Models_DB/ClienteDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Lib.Aula02.Models_DB
+{
+    /// <summary>
+    /// Classe que representa o cliente no banco de dados
+    /// </summary>
+    public class ClienteDB
+    {
+        /// <summary>
+        /// Código do Cliente
+        /// </summary>
+        public int Codigo { get; set; }
+
+        /// <summary>
+        /// Nome do Cliente
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF do cliente
+        /// </summary>
+        public string CPF { get; set; }
+    }
+}

a89ac840691fc583f46f61d8ec15fb077b45f16f
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-16 11:06:55 -0300

    Implementação de Validação e CPF + Testes #13 / #8
    Implementação da Validação do CPF + Respectivos testes #13 / #8
    
    Closes #13

---
+++ Aula.UnitTest/Aula02Testes/Exemplo02Testes/TestarValidadeCPF.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Aula.Lib.Aula02;
+
+namespace Aula.UnitTest.Aula02Testes.Exemplo02Testes
+{
+    public class TestarValidadeCPF
+    {
+        [Fact]
+        public void CPFProprio()
+        {
+            bool Valida = Exemplo02.VerificarCPF("365.224.568-77");
+
+            Assert.True(Valida);
+        }
+        [Fact]
+        public void CPF4Devs1()
+        {
+            bool Valida = Exemplo02.VerificarCPF("284.716.780-39");
+
+            Assert.True(Valida);
+        }
+        [Fact]
+        public void CPF4Devs2()
+        {
+            bool Valida = Exemplo02.VerificarCPF("868.587.470-01");
+
+            Assert.True(Valida);
+        }
+        [Fact]
+        public void CPF4Devs3()
+        {
+            bool Valida = Exemplo02.VerificarCPF("983.114.230-67");
+
+            Assert.True(Valida);
+        }
+        [Fact]
+        public void CPFInvalido()
+        {
+            bool Valida = Exemplo02.VerificarCPF("658.874.365-55");
+
+            Assert.False(Valida);
+        }
+    }
+}
---
+++ Aula.lib/Aula02/Exemplo02.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Lib.Aula02
+{
+    /// <summary>
+    /// Classe feita para testar o CPF
+    /// </summary>
+    public class Exemplo02
+    {
+        //Fonte para obter o cálculo do CPF: http://www.macoratti.net/11/09/c_val1.htm
+        //Fonte para entendimento do cálculo: https://campuscode.com.br/conteudos/o-calculo-do-digito-verificador-do-cpf-e-do-cnpj#:~:text=O%20c%C3%A1lculo%20de%20valida%C3%A7%C3%A3o%20do,2%20e%20somamos%20esse%20resultado.
+
+        /// <summary>
+        /// VerificarCPF
+        /// </summary>
+        /// <param name="CPF">CPF da pessoa a ser verificado</param>
+        /// <returns>Retorna se o CPF é válido ou não</returns>
+        public static bool VerificarCPF(string CPF)
+        {
+            //
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCPF;
+            string digito;
+            int soma;
+            int resto;
+            CPF = CPF.Trim();
+            CPF = CPF.Replace(".", "").Replace("-", "").Replace(" ", "");
+            if (CPF.Length != 11)
+                return false;
+            tempCPF = CPF.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCPF[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto > 10)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCPF += digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCPF[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto > 10)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito += resto.ToString();
+
+            return CPF.EndsWith(digito);
+        }
+    }
+}

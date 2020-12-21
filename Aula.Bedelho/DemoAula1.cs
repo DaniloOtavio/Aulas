0f5db4b3f96f83860b26902f87b2c26ca4e43454
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-21 17:58:25 -0300

    Coisas da Aula 04

---
+++ Aula.Bedelho/DemoAula1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Bedelho
+{
+    static class DemoAula1
+    {
+        static void parte1()
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Array");
+            int[] array = { 1, 3, 5, 73, 9 };
+            mostraAGalera(array);
+
+            Console.WriteLine("Lista");
+            List<int> lst = new List<int>() { 1, 3, 5, 73, 9 };
+            mostraAGalera(lst);
+
+
+            void mostraAGalera(IEnumerable<int> galera)
+            {
+                foreach (var g in galera)
+                {
+                    Console.WriteLine($"Maluco: {g}");
+                }
+            }
+
+        }
+
+        static void parte2()
+        {
+            Console.WriteLine("Hello World!");
+
+            var numeros = RetornaTodosOsNumerosParesEntre(13, 17);
+
+            //14, 16
+
+            foreach (var n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+            static List<int> RetornaTodosOsNumerosParesEntre(int inicio, int fim)
+            {
+                List<int> pares = new List<int>();
+
+                for (int i = inicio; i <= fim; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        pares.Add(i);
+                    }
+                }
+                return pares;
+            }
+        }
+    }
+}
--- Aula.Bedelho/Program.cs
+++ Aula.Bedelho/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 Console.WriteLine("Hello World!");
+

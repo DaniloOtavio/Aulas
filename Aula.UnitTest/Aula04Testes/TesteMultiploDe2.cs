61b05fb3e92d9a7101f8a91eec5c8860433d096c
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-22 23:28:04 -0300

    Implementando testes #21
    Correção da Issue #21  com o que foi solicitado
    
    Closes #21

--- Aula.UnitTest/Aula04Testes/TesteMultiploDe2.cs
+++ Aula.UnitTest/Aula04Testes/TesteMultiploDe2.cs
@@ -10,9 +10,32 @@ namespace Aula.UnitTest.Aula04Testes
     public class TesteMultiploDe2
     {
         [Fact]
-        public void TestaNumerosPares()
+        public void TestaNumerosPares1()
         {
-            throw new NotImplementedException();
+            var resultado = ExercicioAula04.NumerosParesEntre(0, 10);
+
+            Assert.Equal(new double[] { 0, 2, 4, 6, 8 }, resultado);
+        }
+        [Fact]
+        public void TestaNumerosPares2()
+        {
+            var resultado = ExercicioAula04.NumerosParesEntre(7, 25);
+
+            Assert.Equal(new double[] { 8,10,12,14,16,18,20,22,24 }, resultado);
+        }
+        [Fact]
+        public void TestaNumerosPares3()
+        {
+            var resultado = ExercicioAula04.NumerosParesEntre(105, 108);
+
+            Assert.Equal(new double[] { 106 }, resultado);
+        }
+        [Fact]
+        public void TestaNumerosPares4()
+        {
+            var resultado = ExercicioAula04.NumerosParesEntre(199, 220);
+
+            Assert.Equal(new double[] { 200, 202, 204, 206, 208, 210, 212, 214, 216, 218 }, resultado);
         }
     }
 }

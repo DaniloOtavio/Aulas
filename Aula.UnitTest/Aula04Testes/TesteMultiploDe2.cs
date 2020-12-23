8c1b79be307a46c477491c4415b11ffdbe2b2455
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2020-12-22 23:06:14 -0300

    Implementando testes Multiplode2 #21

---
+++ Aula.UnitTest/Aula04Testes/TesteMultiploDe2.cs
@@ -0,0 +1,38 @@
+﻿using Aula.Lib.Aula04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Aula.UnitTest.Aula04Testes
+{
+    public class TesteMultiploDe2
+    {
+        [Fact]
+        public void TestaMultiploDe2Pares()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void TestaMultiploDe2Impares()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void TestaMultiploDe2ParImpar()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void TestaMultiploDe2ImparPar()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void TestaMultiploDe2InicioMaiorQueFim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aula.lib/Aula04/ExercicioAula04.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Lib.Aula04
+{
+    /// <summary>
+    /// Exercícios Aula01
+    /// </summary>
+    public static class ExercicioAula04
+    {
+        /// <summary>
+        /// Função para verificar números pares entre 2 valores
+        /// </summary>
+        /// <param name="Inicio">Início do período</param>
+        /// <param name="Fim">Fim do período</param>
+        /// <returns>Retorna números pares</returns>
+        public static double[] NumerosParesEntre(int Inicio, int Fim)
+        {
+            if (Inicio % 2 == 0 && Fim % 2 != 0)
+            {
+                return Array.Empty<double>();
+            }
+            else if(Inicio % 2 != 0 && Fim % 2 == 0)
+            {
+                return Array.Empty<double>();
+            }
+            else if (Inicio >= Fim)
+            {
+                return Array.Empty<double>();
+            }
+            double[] numerosPares = new double[(Fim - Inicio) / 2];
+            int pos = 0;
+
+            for (int i = Inicio; i < Fim; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numerosPares[pos] = i;
+                    _ = pos++;
+                }
+            }
+            return numerosPares;
+        }
+
+
+    }
+}

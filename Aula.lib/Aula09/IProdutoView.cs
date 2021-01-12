fe61f83b6bad43dec83b6d3821ea79ff3c314aaa
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-12 19:11:52 -0300

    Create IProdutoView.cs

---
+++ Aula.lib/Aula09/IProdutoView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aula.Lib.Aula08;
+
+namespace Aula.Lib.Aula09
+{
+    public interface IProdutoView
+    {
+        void CadastrarProduto(string Key, ProdutoCadastro Produto);
+        string[] ListarTodasKeys();
+        ProdutoCadastro BuscarProduto(string key);
+        void Setup();
+    }
+}

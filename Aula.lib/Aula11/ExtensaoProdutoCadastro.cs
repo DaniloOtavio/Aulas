75aea7ca9a32dfee737b0b1d9022d09c86ce9aa8
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2021-01-24 17:12:03 -0300

    Implantação Extensão ProdutosAlternados #60

--- Aula.lib/Aula11/ExtensaoProdutoCadastro.cs
+++ Aula.lib/Aula11/ExtensaoProdutoCadastro.cs
@@ -15,7 +15,7 @@ namespace Aula.Lib.Aula11
         /// <summary>
         /// Buscar produto por parte do nome
         /// </summary>
-        /// <param name="produtos">Produto</param>
+        /// <param name="produtos">Produtos</param>
         /// <param name="parteNome">Parte do nome</param>
         /// <returns>Retorna o produto com base no nome</returns>
         public static IEnumerable<ProdutoCadastro> FiltroNome(this IEnumerable<ProdutoCadastro> produtos, string parteNome)
@@ -27,6 +27,24 @@ namespace Aula.Lib.Aula11
                     yield return prod;
                 }
 
+            }
+        }
+        /// <summary>
+        /// Função para consultar produtos intercalados
+        /// </summary>
+        /// <param name="produtos">Produtos</param>
+        /// <returns>Retorna os produtos de forma intercalada</returns>
+        public static IEnumerable<ProdutoCadastro> ProdutosAlternados(this IEnumerable<ProdutoCadastro> produtos)
+        {
+            bool pula = false;
+            foreach (var prod in produtos)
+            {
+                if (pula == false)
+                {
+                    yield return prod;
+                    pula = true;
+                }
+                else pula = false;
             }
         }
     }

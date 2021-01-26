332d84a4a8a17c878f7917bd0439433b62e0620f
DaniloOtavio <75633993+DaniloOtavio@users.noreply.github.com>
2021-01-26 19:58:37 -0300

    Criação da função genérica de filtro #66
    Closes #66

--- Aula.lib/Aula13/Extensions.cs
+++ Aula.lib/Aula13/Extensions.cs
@@ -1,9 +1,27 @@
-﻿namespace Aula.Lib.Aula13
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula.Lib.Aula13
 {
     /// <summary>
     /// Classe de extensões genéricas
     /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Filtro genérico
+        /// </summary>
+        /// <typeparam name="T">Tipo do objeto</typeparam>
+        /// <param name="itens">Itens a serem filtrados</param>
+        /// <param name="regra">Regra</param>
+        /// <returns>Retorna itens</returns>
+        public static IEnumerable<T> Filtrar<T>(this IEnumerable<T> itens, Func<T,bool> regra)
+        {
+            foreach (var it in itens)
+            {
+                if (!regra(it)) continue;
+                yield return it;
+            }
+        }
     }
 }
